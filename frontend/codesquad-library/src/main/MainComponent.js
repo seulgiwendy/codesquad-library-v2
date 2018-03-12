bb1fdf0ebb184ca2141355999840b11831e49485
seulgiwendy <wheejuni@gmail.com>
2018-03-12 19:07:05 +0900

    [main][navbar] navbar 기본 컨셉 구현

--- frontend/codesquad-library/src/main/MainComponent.js
+++ frontend/codesquad-library/src/main/MainComponent.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react';
-import {Navbar, SideNav, Button} from 'react-materialize';
+import {Navbar, NavItem, MenuItem, NavDropdown, Nav} from 'react-bootstrap';
 
 class MainComponent extends Component {
     render() {
         return (
-            <div className="component">
-                <Navbar brand = '&nbsp;&nbsp;Codesquad-Library' right/>
+            <div className="main">
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            Codesquad Library
+                        </Navbar.Brand>
+                    </Navbar.Header>
+                    <Nav>
+                        <NavItem href="#"> 도서검색 </NavItem>
+                        <NavItem href="#"> 도서관리 </NavItem>
+                        <NavItem href="#"> 대출정보 </NavItem>
+                    </Nav>
+                </Navbar>
             </div>
         )
     }
