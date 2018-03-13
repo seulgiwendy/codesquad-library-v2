3e3603f08a37878f5fb35b728ea23b1e3e907d7b
seulgiwendy <wheejuni@gmail.com>
2018-03-13 11:04:30 +0900

    [main]메인화면 구성, inline style을 CSS로 옮김

--- frontend/codesquad-library/src/main/MainComponent.js
+++ frontend/codesquad-library/src/main/MainComponent.js
@@ -1,42 +1,38 @@
 import React, {Component} from 'react';
-import {Navbar, NavItem, MenuItem, NavDropdown, Nav, Jumbotron} from 'react-bootstrap';
-import '../App.css'
-
-const styles = {
-    navbar: {
-        backgroundColor : 'black',
-        margin : 0
-    },
-    navbarLetter : {
-        color : 'white'  
-    }
-}
-
+import {Navbar, NavItem, MenuItem, NavDropdown, Nav, Jumbotron, Button} from 'react-bootstrap';
+import './main.css'
 
 class MainComponent extends Component {
     render() {
         return (
             <div className="main">
-                <Navbar style = {styles.navbar}>
+                <Navbar className="navbar-default">
                     <Navbar.Header>
                         <Navbar.Brand>
                             Codesquad Library
                         </Navbar.Brand>
                     </Navbar.Header>
-                    <Nav>
-                            <NavItem id = "navbar-menu" href="#"> 도서검색 </NavItem>
-                            <NavItem id = "navbar-menu" href="#"> 도서관리 </NavItem>
-                            <NavItem id = "navbar-menu" href="#"> 대출정보 </NavItem>
-                        
+                    <Nav className="navbar-letter">
+                            <NavItem id = "navbar-menu" activehref="#"> 도서검색 </NavItem>
+                            <NavItem id = "navbar-menu" activehref="#"> 도서관리 </NavItem>
+                            <NavItem id = "navbar-menu" activehref="#"> 대출정보 </NavItem>
+                    </Nav>
+                    <Nav className="navbar-personal" pullRight>
+                            <Button bsStyle = "primary" activehref="#" id = "navbar-button"> Login </Button>
                     </Nav>
                 </Navbar>
                 <div className="main-body">
                     <Jumbotron className = "main-jumbo">
-                        <h1 id="jumbotron-h1">hello world</h1>
+                        <h1 id="jumbotron-h1">코드스쿼드 도서관</h1>
                         <p>
-                            코드스쿼드의 도서 관리와 대여를 위한 페이지입니다.
+                            코드스쿼드의 도서 관리와 대여를 위한 페이지입니다. <br/>
+                            책을 많이 읽읍시다.
                         </p>
                     </Jumbotron>
+                    <br/>
+                    <div className="main-body-lower">
+                        <h1 id="main-h1">책을 많이 읽읍시다.</h1>
+                    </div>
                 </div>
             </div>
         
