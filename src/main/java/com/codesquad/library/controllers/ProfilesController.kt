b24805579aa87bda14897404b01714bacb4492b3
seulgiwendy <wheejuni@gmail.com>
2018-03-22 09:40:10 +0900

    [project]Profiles Controller 추가, 코틀린 필수의존성 추가

---
+++ src/main/java/com/codesquad/library/controllers/ProfilesController.kt
@@ -0,0 +1,14 @@
+package com.codesquad.library.controllers
+
+import org.springframework.core.env.Environment
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ProfilesController constructor(var environment: Environment) {
+
+    @GetMapping("/profile")
+    fun getProfile() : String {
+        return environment.activeProfiles.getOrElse(0) {"no profiles found!"}
+    }
+}
